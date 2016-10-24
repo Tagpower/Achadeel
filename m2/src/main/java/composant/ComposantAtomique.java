eb31f1248c6f5e02c1c8c0ab8d2f04b22a4879d3
clement <clem.ssbm@hotmail.fr>
2016-10-24 14:44:51 +0200

    No more interfaces

--- m2/src/main/java/composant/ComposantAtomique.java
+++ m2/src/main/java/composant/ComposantAtomique.java
@@ -7,32 +7,10 @@ public class ComposantAtomique extends Composant {
 
     //    private List<Propriete> props;
     //    private List<Contrainte> contr;
-    private InterfaceComposantRequis i_requis;
-    private InterfaceComposantFourni i_fourni;
     private String nom;
 
-    public ComposantAtomique(String nom, InterfaceComposantRequis requis, InterfaceComposantFourni fourni) {
+    public ComposantAtomique(String nom) {
         this.nom = nom;
-        i_requis = requis;
-        i_fourni = fourni;
-    }
-
-
-
-    public InterfaceComposantFourni getI_fourni() {
-        return i_fourni;
-    }
-
-    public void setI_fourni(InterfaceComposantFourni i_fourni) {
-        this.i_fourni = i_fourni;
-    }
-
-    public InterfaceComposantRequis getI_requis() {
-        return i_requis;
-    }
-
-    public void setI_requis(InterfaceComposantRequis i_requis) {
-        this.i_requis = i_requis;
     }
 
 
