f30f83a7004ec1b63aa52832ad008b24d5d8fd15
naixin <wangnaixinensim@gmail.com>
2016-11-07 16:03:24 +0100

    des constructeurs de composants et connecteurs dans config serveur

--- m1/src/main/java/serveur/DBMgr/DatabaseManager.java
+++ m1/src/main/java/serveur/DBMgr/DatabaseManager.java
@@ -2,9 +2,18 @@ package serveur.DBMgr;
 
 import composant.Composant;
 import composant.ComposantAtomique;
+import serveur.connectionMgr.SecurityCheck_in;
+import serveur.connectionMgr.SecurityCheck_out;
 
 /**
  * Created by clement on 06/11/16.
  */
 public class DatabaseManager extends ComposantAtomique {
+    public DatabaseManager(String nom) {
+        super(nom);
+        this.addPortRequis(new QueryD_in(this,"QueryDataIn"));
+        this.addPortsFourni(new QueryD_out(this, "QueryDataOut"));
+        this.addPortRequis(new SecurityCheck_in(this, "SecurityManagementIn"));
+        this.addPortsFourni(new SecurityCheck_out(this, "SecurityManagementOut"));
+    }
 }
